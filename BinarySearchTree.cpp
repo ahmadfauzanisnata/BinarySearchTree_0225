88eef2ca25917fdca24b83420113c66d07a1d480
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-06-13 15:19:46 +0700

    make the left child of the new node point to null

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -30,6 +30,7 @@ class BinaryTree
     {
         Node* newNode = new Node(element, nullptr, nullptr); // allocate memory for the new node
         newNode->info = element; // assign value to the data field of the new node
+        newNode->leftchild = nullptr; // make the left child of the new node point to null
 
     }
 }
