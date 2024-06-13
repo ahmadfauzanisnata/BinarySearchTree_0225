75109192b25a4bb0ed098b764f910adc77257329
ahmadfauzanisnata <fauzanisnata56@gmail.com>
2024-06-13 15:30:14 +0700

    make the left child of the parent point to the new node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -45,7 +45,7 @@ class BinaryTree
         }
         if (element < parent->info) // if the value in the data field of the new node is less than that of the
         {
-            
+            parent->leftchild = newNode; // make the left child of the parent point to the new node
         }
 
         
